59ecdc08983111d87d95fc6877fabf19588d7219
Jaya <jaya.vedantam@gmail.com>
2021-07-26 12:44:25 -0700

    added city and temp info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
 /* Api call to weather for a search City */
 var recentCitySearch = document.querySelector("#recent-search");
 var todaysWeather = document.querySelector("#todays-weather");
+var show5DayHeader = document.querySelector("#show-five-day-header");
+var display5DayWeather = document.querySelector("#five-day-forecast");
 var saveCity = [];
 var apiKey = "a65c5c15039d05a0f7175619a4cb05e1";
 var tempUnit = "imperial";
+var cityId;
+
+var displayCityDateInfo = document.getElementById("selected-city");
+var temperature = document.getElementById("temperature");
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
+var uvIndex = document.getElementById("UV-index");
 
 function searchCity() {
   /* Remove hide class show Recent Search div after search is clicked */
   recentCitySearch.classList.remove("hide");
   recentCitySearch.classList.add("show");
   todaysWeather.classList.remove("hide");
-  todaysWeather.classList.add("show");  
+  todaysWeather.classList.add("show");
+  show5DayHeader.classList.remove("hide");
+  show5DayHeader.classList.add("show");
+  display5DayWeather.classList.remove("hide");
+  display5DayWeather.classList.add("show");
 
   /* Get City and convert first letter to upper case */
   var getCity = document.querySelector("#city").value;
@@ -34,30 +47,78 @@ function weatherAPIRequest(cityName) {
       return response.json();
     })
     .then(function (response) {
-           
       /* fORMAT dATE */
       var currentDate = JSON.stringify(response.list[0].dt_txt);
       currentDate = currentDate.slice(1, -1);
       currentDate = currentDate.split(" ")[0];
-    
-      var displayCurrentWeather = document.createElement("h1");
-      displayCurrentWeather.textContent = `${cityName} (${currentDate})`;
 
-      todaysWeather.appendChild(displayCurrentWeather);
+      /* Weather icon*/
+      var weatherIcon = JSON.stringify(response.list[0].weather[0].icon);
+
+      displayCityDateInfo.textContent =
+        `${cityName} (${currentDate}) ${weatherIcon}` + "@2x.png";
+      todaysWeather.appendChild(displayCityDateInfo);
+
       /* Display weather conditions */
-      var temperature = JSON.stringify(response.list[0].main.temp);
-      var displayTemperature = document.createElement("p");
-      displayTemperature.classList.add("weather-conditions");
-      displayTemperature.textContent = `Temp: ${temperature}F`;
-      
-      var displayWind = document.createElement("p");
-      displayWind.classList.add("weather-conditions");
-      displayWind.textContent ="something";
+      var responseTemperature = JSON.stringify(response.list[0].main.temp);
+      temperature.textContent = `Temp: ${responseTemperature}F`;
 
-      todaysWeather.appendChild(displayTemperature);
-      todaysWeather.appendChild(displayTemperature);
+      var responseWind = JSON.stringify(response.list[0].wind.speed);
+      wind.textContent = `Wind: ${responseWind} MPH`;
+
+      var responseHumidity = JSON.stringify(response.list[0].main.humidity);
+      humidity.textContent = `Humidity: ${responseHumidity} %`;
+
+      // Get UV Index
+      var lat = response.city.coord.lat;
+      var lon = response.city.coord.lon;
+      cityId = response.city.id;
+
+      var UVQueryURL = `https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&cnt=1`;
+
+      fetch(UVQueryURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          console.log("resp ", response[0].value);
+          var responseUVIndex = document.createElement("span");
+          if (response[0].value < 4) {
+            responseUVIndex.setAttribute("class", "badge badge-success");
+          } else if (response[0].value < 8) {
+            responseUVIndex.setAttribute("class", "badge badge-warning");
+          } else {
+            responseUVIndex.setAttribute("class", "badge badge-danger");
+          }
+          responseUVIndex.innerHTML = response[0].value;
+          uvIndex.textContent = `UV-Index: `;
+          uvIndex.appendChild(responseUVIndex);
+        });
+
+      todaysWeather.appendChild(temperature);
+      todaysWeather.appendChild(wind);
+      todaysWeather.appendChild(humidity);     
+      todaysWeather.appendChild(uvIndex);
+      fiveDayWeatherforecast(cityId);
     });
 }
+
+function fiveDayWeatherforecast(cityID){
+  var forecastweatherAPI = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${apiKey}`;
+  console.log('forecast Url' , forecastweatherAPI);
+  fetch(forecastweatherAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    console.log("resp ", response);
+    var fiveDayList = response.list;
+    console.log("fiveDayList", fiveDayList);
+    var forecastFormattedDate = document.createElement("p");
+   // forecastFormattedDate.innerHTML = 
+})
+}
+
 
 function displayRecentSearch() {
   var storedNames = localStorage.getItem("names");

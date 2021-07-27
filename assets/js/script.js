66e31206b380d1fb4829563b9b1d84339f6d96e4
Jaya <jaya.vedantam@gmail.com>
2021-07-26 17:14:45 -0700

    after 5 day weather fore cast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 var recentCitySearch = document.querySelector("#recent-search");
 var todaysWeather = document.querySelector("#todays-weather");
 var show5DayHeader = document.querySelector("#show-five-day-header");
-var display5DayWeather = document.querySelector("#five-day-forecast");
+var weatherForecast = document.querySelector("#weather-forecast");
+
 var saveCity = [];
 var apiKey = "a65c5c15039d05a0f7175619a4cb05e1";
 var tempUnit = "imperial";
@@ -22,8 +23,6 @@ function searchCity() {
   todaysWeather.classList.add("show");
   show5DayHeader.classList.remove("hide");
   show5DayHeader.classList.add("show");
-  display5DayWeather.classList.remove("hide");
-  display5DayWeather.classList.add("show");
 
   /* Get City and convert first letter to upper case */
   var getCity = document.querySelector("#city").value;
@@ -55,8 +54,9 @@ function weatherAPIRequest(cityName) {
       /* Weather icon*/
       var weatherIcon = JSON.stringify(response.list[0].weather[0].icon);
 
-      displayCityDateInfo.textContent =
-        `${cityName} (${currentDate}) ${weatherIcon}` + "@2x.png";
+      // displayCityDateInfo.textContent =
+      //   `${cityName} (${currentDate}) ${weatherIcon}` + "@2x.png";
+      displayCityDateInfo.textContent = `${cityName} (${currentDate})`;
       todaysWeather.appendChild(displayCityDateInfo);
 
       /* Display weather conditions */
@@ -97,28 +97,74 @@ function weatherAPIRequest(cityName) {
 
       todaysWeather.appendChild(temperature);
       todaysWeather.appendChild(wind);
-      todaysWeather.appendChild(humidity);     
+      todaysWeather.appendChild(humidity);
       todaysWeather.appendChild(uvIndex);
       fiveDayWeatherforecast(cityId);
     });
 }
 
-function fiveDayWeatherforecast(cityID){
-  var forecastweatherAPI = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${apiKey}`;
-  console.log('forecast Url' , forecastweatherAPI);
+function fiveDayWeatherforecast(cityID) {
+  var forecastweatherAPI = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${apiKey}&units=imperial`;
+  console.log("forecast Url", forecastweatherAPI);
   fetch(forecastweatherAPI)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    console.log("resp ", response);
-    var fiveDayList = response.list;
-    console.log("fiveDayList", fiveDayList);
-    var forecastFormattedDate = document.createElement("p");
-   // forecastFormattedDate.innerHTML = 
-})
-}
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log("resp ", response);
+      var fiveDayList = response.list;
+      console.log("fiveDayList", fiveDayList);
 
+      /* For each date, there are 8 objects, iterate over the list of 40 items */
+      for (index = 0; index < 5; index++) {
+       /* get Div tag by id and clear content before appending*/
+        var dailyforecast = document.querySelector(
+          `#displayDay${index + 1}Weather`
+        );
+        dailyforecast.innerHTML = " ";
+        
+        // calculate index to retreive 4th occurence object to display 3.00pm values
+        calculatedIndex = index * 8 + 4;
+        if (calculatedIndex < fiveDayList.length) {
+          var forecastDate = document.createElement("p");
+          var forecastTemp = document.createElement("p"); 
+          var forecastWind = document.createElement("p");
+          var forecastHumidity = document.createElement("p");
+     
+
+          var getEachDayDate = fiveDayList[calculatedIndex].dt_txt;
+          getEachDayDate = getEachDayDate.slice(1, -1);
+          getEachDayDate = getEachDayDate.split(" ")[0];
+
+          var eachDayTemp = fiveDayList[calculatedIndex].main.temp;
+          console.log("eachDayTemp ", eachDayTemp);
+          var eachDayWind = fiveDayList[calculatedIndex].wind.speed;
+          console.log("eachDayWind ", eachDayWind);
+          var eachDayHumidity = fiveDayList[calculatedIndex].main.humidity;
+          console.log("eachDayHumidity ", eachDayHumidity);
+
+          weatherForecast.classList.remove("hide");
+          weatherForecast.classList.add("show-forecast");
+          weatherForecast.classList.add("weather-forecast");
+
+          forecastDate.classList.add("each-day-date");
+          forecastDate.innerHTML = `(${getEachDayDate})`;
+
+          forecastTemp.innerHTML = `Temp : ${eachDayTemp} F`;
+          console.log(" forecastTemp text content ", forecastTemp.innerHTML);
+
+          forecastWind.innerHTML = `Wind : ${eachDayWind} MPH`;
+
+          forecastHumidity.innerHTML = `Humidity : ${eachDayHumidity} %`;
+        }
+
+        dailyforecast.append(forecastDate.innerHTML);
+        dailyforecast.appendChild(forecastTemp);
+        dailyforecast.appendChild(forecastWind);
+        dailyforecast.appendChild(forecastHumidity);
+      }
+    });
+}
 
 function displayRecentSearch() {
   var storedNames = localStorage.getItem("names");
